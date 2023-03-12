fbffcfa81b21d0ef6cfec17d82afcddf0497e92c
Thalles <thalles_3@hotmail.com>
2023-03-11 22:23:37 -0300

    Finish working and refactored palindrome method

--- test/test_thallel_palindrome.rb
+++ test/test_thallel_palindrome.rb
@@ -3,11 +3,25 @@
 require "test_helper"
 
 class TestThallelPalindrome < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::ThallelPalindrome::VERSION
+  def test_non_palindrome
+    assert !"apple".palindrome?
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_literal_palindrome
+    assert "racecar".palindrome?
+  end
+
+  def test_mixed_case_palindrome
+    assert "RaceCar".palindrome?
+  end
+
+  def test_palindrome_with_punctuation
+    assert "Madam, I'm Adam.".palindrome?
+  end
+
+  def test_letters
+    assert_equal "MadamImAdam", "Madam, I'm Adam.".letters
   end
 end
+  
+
